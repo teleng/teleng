0209a2552b19ae56708f1db75ff02dfb3a6e005a
kiran kumar palli <kiran.palli@gmail.com>
2012-01-21 23:23:56 -0800

    adding public_html and doing some clean up

---
+++ www/public_html/test.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<title>My Sabbatical blog</title>
+<script type="text/javascript">
+
+</script>
+<style type="text/css">
+body
+{
+font-size:12px;
+color:#555555;
+background-color:#ffffff;
+margin-bottom:0px;
+background-image:none;
+}
+
+div
+{
+margin:0;
+padding:0;	
+}
+#maincolumn
+{
+font-size:13px;
+width:780px;
+padding-bottom:8px;
+padding-left:4px;
+float:left
+}
+
+#leftcolumn
+{
+width:170px;
+margin-left:40px;
+margin-top:4px;
+float:left;
+color:blue;
+}
+</style>
+</head>
+<body>
+<div style="position:relative;width:100%;">
+    <div style="width:960px;margin-top:5px;margin-left:auto;margin-right:auto">
+        <div style="width:960px">
+            <div id="leftcolumn">  My fav links<br/>
+	    </div>
+	    <div id="maincolumn">
+	       	<br>
+            	<div style="float:left;padding-right:5px;">
+            	    <iframe width="420" height="315" align="left" src="http://www.youtube.com/embed/j04MtLajPmU" frameborder="0" allowfullscreen> </iframe>
+                </div>       
+	    </div>
+        </div>
+    </div>
+<div>
+</body>
+</html> 
